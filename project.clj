9b337d2519f7f5ff67ea543da6d519c3c7ad6d15
dennis zhuang <killme2008@gmail.com>
2011-12-02 16:01:01 +0800

    Upgrade storm to 0.6.0

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject storm-monitor "1.0.0-SNAPSHOT"
   :description "Storm monitor"
   :extra-classpath-dirs ["conf"]
+  :dev-dependencies [[lein-exec "0.1"]]
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [storm/storm "0.5.4"]
+                 [storm/storm "0.6.0"]
                  [org.yaml/snakeyaml "1.9"]
                  [tron   "0.5.2"]])
